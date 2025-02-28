09b5ca5110c4454b04af756000bf511c23cd8b5d
Humphrey Sun <namesblanks@gmail.com>
2025-03-01 02:30:34 +0800

    perf： refactor history page to mvvm

--- src/HistoryPage.xaml.cs
+++ src/HistoryPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using LiveCaptionsTranslator.controllers;
 using LiveCaptionsTranslator.models;
-using System.Windows;
 using Microsoft.Win32;
 using Wpf.Ui.Controls;
 
@@ -9,116 +12,20 @@ namespace LiveCaptionsTranslator
 {
     public partial class HistoryPage : Page
     {
-        int page = 1;
-        int maxPage = 1;
-        int maxRow = 20;
-
         public HistoryPage()
         {
             InitializeComponent();
-            LoadHistory();
+            var viewModel = new HistoryPageViewModel();
 
-            HistoryMaxRow.SelectedIndex = App.Settings.HistoryMaxRow;
-            TranslationController.TranslationLogged += async () => await LoadHistory();
-        }
-
-        private async Task LoadHistory()
-        {
-            var data = await SQLiteHistoryLogger.LoadHistoryAsync(page, maxRow);
-            List<TranslationHistoryEntry> history = data.Item1;
-
-            maxPage = (data.Item2 > 0) ? data.Item2 : 1;
-
-            await Dispatcher.InvokeAsync(() =>
+            viewModel.ShowSnackbarAction = (title, message, isError) =>
             {
-                HistoryDataGrid.ItemsSource = history;
-                PageNamber.Text = page.ToString() + "/" + maxPage.ToString();
-            });
-        }
-
-        void PageDown(object sender, RoutedEventArgs e)
-        {
-            if (page - 1 >= 1)
-                page--;
-                LoadHistory();
-
-        }
-        void PageUp(object sender, RoutedEventArgs e)
-        {
-            if (page < maxPage)
-                page++;
-                LoadHistory();
-        }
-
-        private async void DeleteHistory(object sender, RoutedEventArgs e)
-        {
-            var dialog = new ContentDialog
-            {
-                Title = "Do you want to delete all history?",
-                PrimaryButtonText = "Yes",
-                CloseButtonText = "No",
-                DefaultButton = ContentDialogButton.Close,
-                DialogHost = (Application.Current.MainWindow as MainWindow)?.DialogHostContainer,
-                Padding = new Thickness(8, 4, 8, 8),
+                ShowSnackbar(title, message, isError);
             };
-
-            var result = await dialog.ShowAsync();
-            if (result == ContentDialogResult.Primary)
-            {
-                page = 1;
-                SQLiteHistoryLogger.ClearHistory();
-                await LoadHistory();
-            }
+            DataContext = viewModel;
         }
 
-        private void maxRow_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ShowSnackbar(string title, string message, bool isError = false)
         {
-            string text = (e.AddedItems[0] as ComboBoxItem).Tag as string;
-            maxRow = Convert.ToInt32(text);
-            App.Settings.HistoryMaxRow = HistoryMaxRow.SelectedIndex;
-
-            LoadHistory();
-
-            if (page > maxPage)
-            {
-                page = maxPage;
-                LoadHistory(); ;
-            }
-        }
-
-        private void ReloadLogs(object sender, RoutedEventArgs e)
-        {
-            LoadHistory();
-        }
-
-        private async void ExportHistory(object sender, RoutedEventArgs e)
-        {
-
-            SaveFileDialog saveFileDialog = new SaveFileDialog
-            {
-                Filter = "CSV (*.csv)|*.csv|All file (*.*)|*.*",
-                DefaultExt = ".csv",
-                FileName = $"exported_{DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")}.csv",
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
-            };
-            
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                try
-                {
-                    await SQLiteHistoryLogger.ExportToCsv(saveFileDialog.FileName);
-                    ShowSnackbar("Saved Success",$"File saved to: {saveFileDialog.FileName}"); 
-                }
-                catch (Exception ex)
-                {
-                    ShowSnackbar("Save Failed",$"File saved faild:{ex.Message}");
-                }
-            }
-        }
-        
-        private void ShowSnackbar(string title,string message, bool isError = false)
-        {
-
             var snackbar = new Snackbar(SnackbarHost)
             {
                 Title = title,
@@ -126,9 +33,230 @@ namespace LiveCaptionsTranslator
                 Appearance = isError ? ControlAppearance.Danger : ControlAppearance.Light,
                 Timeout = TimeSpan.FromSeconds(2)
             };
-
-            snackbar.Show(); 
+            snackbar.Show();
         }
 
+        #region RelayCommand 
+
+        public class RelayCommand : ICommand
+        {
+            private readonly Func<object, Task> _executeAsync;
+            private readonly Action<object> _execute;
+            private readonly Predicate<object> _canExecute;
+
+            public RelayCommand(Func<object, Task> executeAsync, Predicate<object> canExecute = null)
+            {
+                _executeAsync = executeAsync;
+                _canExecute = canExecute;
+            }
+
+            public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+            {
+                _execute = execute;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            public async void Execute(object parameter)
+            {
+                if (_executeAsync != null)
+                {
+                    await _executeAsync(parameter);
+                }
+                else
+                {
+                    _execute?.Invoke(parameter);
+                }
+            }
+        }
+        
+        #endregion
+
+        #region HistoryPageViewModel
+
+        public class HistoryPageViewModel : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+            private void OnPropertyChanged(string propertyName) =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            
+            private int _page = 1;
+            public int Page
+            {
+                get => _page;
+                set { _page = value; OnPropertyChanged(nameof(Page)); UpdatePageNumberText(); }
+            }
+
+            private int _maxPage = 1;
+            public int MaxPage
+            {
+                get => _maxPage;
+                set { _maxPage = value; OnPropertyChanged(nameof(MaxPage)); UpdatePageNumberText(); }
+            }
+
+            private int _maxRow = 20;
+            public int MaxRow
+            {
+                get => _maxRow;
+                set { _maxRow = value; OnPropertyChanged(nameof(MaxRow)); LoadHistoryAsync(); }
+            }
+
+            private string _pageNumberText = "1/1";
+            public string PageNumberText
+            {
+                get => _pageNumberText;
+                set { _pageNumberText = value; OnPropertyChanged(nameof(PageNumberText)); }
+            }
+            
+            private string _selectedMaxRowString = "20";
+            public string SelectedMaxRowString
+            {
+                get => _selectedMaxRowString;
+                set
+                {
+                    if (_selectedMaxRowString != value)
+                    {
+                        _selectedMaxRowString = value;
+                        OnPropertyChanged(nameof(SelectedMaxRowString));
+                        if (int.TryParse(value, out int newMaxRow))
+                        {
+                            MaxRow = newMaxRow;
+                        }
+                        if (value == "20") App.Settings.HistoryMaxRow = 0;
+                        else if (value == "30") App.Settings.HistoryMaxRow = 1;
+                        else if (value == "50") App.Settings.HistoryMaxRow = 2;
+                        else if (value == "100") App.Settings.HistoryMaxRow = 3;
+                    }
+                }
+            }
+
+            private ObservableCollection<TranslationHistoryEntry> _historyEntries = new ObservableCollection<TranslationHistoryEntry>();
+            public ObservableCollection<TranslationHistoryEntry> HistoryEntries
+            {
+                get => _historyEntries;
+                set { _historyEntries = value; OnPropertyChanged(nameof(HistoryEntries)); }
+            }
+            
+            public Action<string, string, bool> ShowSnackbarAction { get; set; }
+            public ICommand PageDownCommand { get; }
+            public ICommand PageUpCommand { get; }
+            public ICommand ReloadLogsCommand { get; }
+            public ICommand DeleteHistoryCommand { get; }
+            public ICommand ExportHistoryCommand { get; }
+
+            public HistoryPageViewModel()
+            {
+                switch (App.Settings.HistoryMaxRow)
+                {
+                    case 1: _selectedMaxRowString = "30"; break;
+                    case 2: _selectedMaxRowString = "50"; break;
+                    case 3: _selectedMaxRowString = "100"; break;
+                    default: _selectedMaxRowString = "20"; break;
+                }
+                _maxRow = int.Parse(_selectedMaxRowString);
+
+                PageDownCommand = new RelayCommand(async _ =>
+                {
+                    if (Page > 1)
+                    {
+                        Page--;
+                        await LoadHistoryAsync();
+                    }
+                });
+
+                PageUpCommand = new RelayCommand(async _ =>
+                {
+                    if (Page < MaxPage)
+                    {
+                        Page++;
+                        await LoadHistoryAsync();
+                    }
+                });
+
+                ReloadLogsCommand = new RelayCommand(async _ =>
+                {
+                    await LoadHistoryAsync();
+                });
+
+                DeleteHistoryCommand = new RelayCommand(async _ =>
+                {
+                    var dialog = new ContentDialog
+                    {
+                        Title = "Do you want to delete all history?",
+                        PrimaryButtonText = "Yes",
+                        CloseButtonText = "No",
+                        DefaultButton = ContentDialogButton.Close,
+                        DialogHost = (Application.Current.MainWindow as MainWindow)?.DialogHostContainer,
+                        Padding = new Thickness(8, 4, 8, 8)
+                    };
+
+                    var result = await dialog.ShowAsync();
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        Page = 1;
+                        SQLiteHistoryLogger.ClearHistory();
+                        await LoadHistoryAsync();
+                    }
+                });
+
+                ExportHistoryCommand = new RelayCommand(async _ =>
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog
+                    {
+                        Filter = "CSV (*.csv)|*.csv|All file (*.*)|*.*",
+                        DefaultExt = ".csv",
+                        FileName = $"exported_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv",
+                        InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+                    };
+
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        try
+                        {
+                            await SQLiteHistoryLogger.ExportToCsv(saveFileDialog.FileName);
+                            ShowSnackbarAction?.Invoke("Saved Success", $"File saved to: {saveFileDialog.FileName}", false);
+                        }
+                        catch (Exception ex)
+                        {
+                            ShowSnackbarAction?.Invoke("Save Failed", $"File save failed: {ex.Message}", true);
+                        }
+                    }
+                });
+                TranslationController.TranslationLogged += async () => await LoadHistoryAsync();
+                _ = LoadHistoryAsync();
+            }
+
+            private async Task LoadHistoryAsync()
+            {
+                var data = await SQLiteHistoryLogger.LoadHistoryAsync(Page, MaxRow);
+                var list = data.Item1;
+                MaxPage = (data.Item2 > 0) ? data.Item2 : 1;
+
+                if (Page > MaxPage)
+                {
+                    Page = MaxPage;
+                    data = await SQLiteHistoryLogger.LoadHistoryAsync(Page, MaxRow);
+                    list = data.Item1;
+                }
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    HistoryEntries = new ObservableCollection<TranslationHistoryEntry>(list);
+                });
+            }
+
+            private void UpdatePageNumberText()
+            {
+                PageNumberText = $"{Page}/{MaxPage}";
+            }
+        }
+
+        #endregion
     }
 }
